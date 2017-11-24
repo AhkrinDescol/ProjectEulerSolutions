39e16d42e63984e67e0cc1947fa76094aa188b84
Ahkrin Descol <amorley525@hotmail.co.uk>
2017-11-24 15:42:56 +0000

    Ambiguous call to ceil needs sorting out in main.cpp.

--- Problem 3/Problem 3/main.cpp
+++ Problem 3/Problem 3/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
-#include <vector>
+#include <cmath>
 
 // TASK: Find the largest prime factor of 600851475143.
 
 int main()
 {
-	
+	constexpr long int subjectNumber{600851475143};
+	constexpr long int subNumRoot{std::ceil(std::sqrt(subjectNumber))};
+
+	std::cout << subjectNumber << std::endl;
 	return 0;
 }
