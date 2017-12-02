5acd64b9a82a8d8626eb0b3e25773cee502f08a8
AhkrinDescol <amorley525@hotmail.co.uk>
2017-12-02 03:18:24 +0000

    Began Problem 13: Large sum

---
+++ Problem 13/Problem 13/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+// TODO: Work of the first ten digits of the sum of the following
+// 	 one-hundred 50-digit numbers.
+
+int main()
+{
+	std::cout << std::numeric_limits<unsigned long long int>::max() << std::endl;
+	
+	return 0;
+}
