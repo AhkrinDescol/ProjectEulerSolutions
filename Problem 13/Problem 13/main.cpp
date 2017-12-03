1acaddffb33ea21488a09966601bd553661cfbe6
AhkrinDescol <amorley525@hotmail.co.uk>
2017-12-03 02:06:47 +0000

    Commiting before attempting first-fifteen-digit ifstream method.

--- Problem 13/Problem 13/main.cpp
+++ Problem 13/Problem 13/main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <vector>
+#include <string>
 
 // TODO: Work of the first ten digits of the sum of the following
 // 	 one-hundred 50-digit numbers.
 
+std::vector<unsigned long long int> gatherAndTruncateValues(std::string fileName);
+
+
 int main()
 {
-	std::cout << std::numeric_limits<unsigned long long int>::max() << std::endl;
+	
 	
 	return 0;
+}
+
+std::vector<unsigned long long int> gatherAndTruncateValues(std::string fileName)
+{
+	std::ifstream inputStream{fileName};
+	std::vector<unsigned long long int> truncatedValues;
+	std::string numberAsString;
+	std::getline(inputStream, numberAsString);
 }
