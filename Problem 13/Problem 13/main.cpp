38c4743a3e2601fcb8e322acd91f5e64a35bd68d
AhkrinDescol <amorley525@hotmail.co.uk>
2017-12-03 04:50:44 +0000

    Completed Problem 13: Large sum.

--- Problem 13/Problem 13/main.cpp
+++ Problem 13/Problem 13/main.cpp
@@ -4,18 +4,29 @@
 #include <string>
 #include <algorithm>
 
-// TODO: Work of the first ten digits of the sum of the following
+// TODO: Work out the first ten digits of the sum of the following
 // 		 one-hundred 50-digit numbers.
 
 std::vector<std::string> gatherValuesAsStrings(std::string fileName);
 std::string sumNumberStrings(const std::vector<std::string>& valuesAsStrings);
-std::string sumTwoNumberStrings(const std::string& lhs, const std::string& rhs);
+std::string sumTwoNumberStrings(std::string lhs, std::string rhs);
 
 int main()
 {
 	const std::string fileName{"Values File.txt"};
 	auto valuesAsStrings{gatherValuesAsStrings(fileName)};
-	
+	std::string summedStrings{sumNumberStrings(valuesAsStrings)};
+	std::cout << "The first ten digits are: ";
+	for (size_t i{0}; i < 10; ++i)
+	{
+		std::cout << summedStrings[i];
+	}
+	std::cout << std::endl;
+	std::cout << "And the remainder are: ";
+	for (size_t i{10}; i < summedStrings.size(); ++i)
+	{
+		std::cout << summedStrings[i];
+	}
 	return 0;
 }
 
@@ -34,47 +45,39 @@ std::vector<std::string> gatherValuesAsStrings(std::string fileName)
 std::string sumNumberStrings(const std::vector<std::string>& valuesAsStrings)
 {
 	std::string sum;
-	for (const auto value : valuesAsStrings)
+	for (auto value : valuesAsStrings)
 	{
 		sum = sumTwoNumberStrings(sum, value);
 	}
 	return sum;
 }
 
-std::string sumTwoNumberStrings(const std::string& lhs, const std::string& rhs)
+std::string sumTwoNumberStrings(std::string lhs, std::string rhs)
 {
-	std::string sum;
-	auto longerIndex{std::max(lhs.size(), rhs.size())};
-	bool carryOne{false};
-	for (int i{0}; i < longerIndex; ++i)
+	if (lhs.size() < rhs.size())
 	{
-		std::string singleAddition;
-		if (i > lhs.size())
-		{
-			singleAddition = rhs[rhs.size() - i];
-		}
-		else if (i > rhs.size())
-		{
-			singleAddition = lhs[lhs.size() - i];
-		}
-		else
-		{
-			const int lhsAsInt{lhs[lhs.size() - i] - '0'};
-			const int rhsAsInt{rhs[rhs.size() - i] - '0'};
-			int sumAsInt{lhsAsInt + rhsAsInt};
-			if (carryOne)
-			{
-				++sumAsInt;
-				carryOne = false;
-			}
-			if (sumAsInt > 9)
-			{
-				carryOne = true;
-			}
-			singleAddition += std::to_string(sumAsInt);
-		}
-		sum.push_back(singleAddition.back());
+		std::swap(lhs, rhs);
 	}
-	
-	return sum;
+	for (int i(lhs.size() - 1), j(rhs.size() - 1); j >= 0; --i, --j)
+	{
+		lhs[i] += (rhs[j] - '0');
+	}
+	for (int i(lhs.size() - 1); i > 0; --i)
+	{
+		if (lhs[i] > '9')
+		{
+			int digit{lhs[i] - '0'};
+			lhs[i - 1] = ((lhs[i - 1] - '0') + digit / 10) + '0';
+			lhs[i] = (digit % 10) + '0';
+		}
+	}
+	if (lhs[0] > '9')
+	{
+		std::string subLhs;
+		subLhs += lhs[0];
+		lhs[0] = ((lhs[0] - '0') % 10) + '0';
+		subLhs[0] = ((subLhs[0] - '0') / 10) + '0';
+		lhs = subLhs + lhs;
+	}
+	return lhs;
 }
